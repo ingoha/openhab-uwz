ae255ebe6a8a2751e3aab5ac073b6734338e980c
Ingo Haschler <ih@ingohaschler.de>
2015-08-14 19:58:33 +0200

    Initial version of JVM spike.

---
+++ uwzWarningSpike.java
@@ -0,0 +1,8 @@
+import java.xml.parsers.*;
+import org.w3c.dom.*;
+
+public static void main() {
+	Document document = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(input));
+	return 0;
+}
+
