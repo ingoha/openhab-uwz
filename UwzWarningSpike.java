66fe0428a2e728fe1268fbe500401df0a81ea5eb
Ingo Haschler <ih@ingohaschler.de>
2015-08-14 20:04:00 +0200

    The memories are flowing :-)

--- UwzWarningSpike.java
+++ UwzWarningSpike.java
@@ -3,10 +3,10 @@ import org.w3c.dom.*;
 
 public class UwzWarningSpike {
 
-	public static void main() {
+	public static void main(String[] args) throws Exception {
 		String input = "<html><head><title>Test</title></head><body><h1>Test</h1></body></html>";
 		Document document = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(input);
-		return 0;
+		System.exit(0);
 	}
 }
 
