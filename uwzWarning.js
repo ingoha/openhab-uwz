1991c409c336ecf5e7572c4bc500dabb55a740a0
Ingo Haschler <ih@ingohaschler.de>
2015-08-14 19:57:25 +0200

    Should test in JVM first...

--- uwzWarning.js
+++ uwzWarning.js
@@ -1,3 +1,9 @@
+// ok, we are running inside rhino here...
+// see https://github.com/openhab/openhab/raw/master/bundles/core/org.openhab.core.transform/src/main/java/org/openhab/core/transform/internal/service/JavaScriptTransformationService.java
+// and https://docs.oracle.com/javase/7/docs/technotes/guides/scripting/programmer_guide/
 (function() {
+	importPackage(java.xml.parsers.*);
+	importPackage(org.w3c.dom.*);
+	var document = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(input));
 	return 0;
-}())
+}(input))
