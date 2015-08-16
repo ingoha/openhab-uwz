22ace5ef136151931e85e0828cd933d0245299c3
Ingo Haschler <ih@ingohaschler.de>
2015-08-16 15:31:00 +0200

    First working version :-)

--- uwzWarning.js
+++ uwzWarning.js
@@ -4,17 +4,26 @@
 
 // logic from http://knx-user-forum.de/forum/%C3%B6ffentlicher-bereich/knx-eib-forum/code-schnipsel/16534-unwetterzentrale
 (function() {
-	var lines = input.split('\n');
-	var regex = '/<div style="float:left;display:block;width:117px;height:110px;padding-top:6px;"><img src="..\/images\/icons\/(.*?)-(.*?).gif" width="117" height="104"><\/div>/i'
+	var lines = input.toString().split('\n');
+	var regex = /<div style="float:left;display:block;width:117px;height:110px;padding-top:6px;"><img src="..\/images\/icons\/(.*?)-(.*?).gif" width="117" height="104"><\/div>/ig;
 
+	var warnstufen = {gelb: 1, orange: 2, rot: 3, violett: 4};
+	var	warntyp = {gewitter: 1, glatteisregen: 2, regen: 3, schnee: 4, sturm: 5, temperatur: 6};
+	
 	var type_max = 0;
 	var level_max = 0;
 	for (var i = 0; i < lines.length; i += 1) {
-		var type = (regex.exec(lines[i]) && regex.exec(lines[i]).$1) || type_max;
-		var level = (regex.exec(lines[i]) && regex.exec(lines[i]).$2) || level_max;
-		if (type > type_max) type_max = type;
-		if (level > level_max) type_max = type;
+		var match = regex.exec(lines[i]);
+		if (match != null) {
+			console.log('Match at line ' + i);
+			console.log(match[1]);
+			var type = warntyp[match[1]];
+			var level = warnstufen[match[2]];
+			if (type > type_max) type_max = type;
+			if (level > level_max) type_max = type;
+		}
 	}
+	console.log('Type ' + type + ', level ' + level);
 	// TBD: how to return warning level?
 	return type_max;
 }(input))
