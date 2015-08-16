b1b1cd867497df07685e3f88465cc28dbb901924
Ingo Haschler <ih@ingohaschler.de>
2015-08-16 15:34:04 +0200

    Fixed multiple typos

--- uwzWarning.js
+++ uwzWarning.js
@@ -16,14 +16,14 @@
 		var match = regex.exec(lines[i]);
 		if (match != null) {
 			console.log('Match at line ' + i);
-			console.log(match[1]);
+			console.log(match[1] + '-' + match[2]);
 			var type = warntyp[match[1]];
 			var level = warnstufen[match[2]];
 			if (type > type_max) type_max = type;
-			if (level > level_max) type_max = type;
+			if (level > level_max) level_max = level;
 		}
 	}
-	console.log('Type ' + type + ', level ' + level);
+	console.log('Type ' + type_max + ', level ' + level_max);
 	// TBD: how to return warning level?
 	return type_max;
 }(input))
